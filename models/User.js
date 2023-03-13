dbeb83f913eb7189627f906e287d523880097a71
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-13 11:43:19 -0400

    Update User.js

--- models/User.js
+++ models/User.js
@@ -42,19 +42,4 @@ User.init(
           newUserData.password = await bcrypt.hash(newUserData.password, 10);
           return newUserData;
       },
-        // set up beforeUpdate lifecycle "hook" 
-      async beforeUpdate(updatedUserData) {
-          updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
-          return updatedUserData;
-      }
-    },
-
-    sequelize,
-    timestamps: false,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'user',
-  }
-);
-
-module.exports = User;
+        
