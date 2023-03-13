1f554d659fcf587d9a16dfdfcf98e5b5ff5258ac
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-13 11:55:55 -0400

    Update server.js

--- server.js
+++ server.js
@@ -29,18 +29,3 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-// turn on routes
-app.use(routes);
-
-// turn on connection to db and server
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('NOW LISTENING!!'));
-});
