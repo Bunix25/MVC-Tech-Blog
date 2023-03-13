5158075872e6bb4b61f1d8870d034723d2d4258f
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-13 11:44:35 -0400

    Update index.js

--- models/index.js
+++ models/index.js
@@ -2,21 +2,25 @@ const User = require('./User');
 const Blog = require('./Blog');
 const Comment = require('./Comment');
 
+
 //associations
 User.hasMany(Blog, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+
 Blog.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
 
+
 Comment.belongsTo(User, {
   foreignKey: 'user_id',
   onDelete: 'CASCADE'
 });
+
 
 Comment.belongsTo(Blog, {
   foreignKey: 'blog_id',
