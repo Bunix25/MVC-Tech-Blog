07d163ab92208a3894656fc42263634017fa544d
Bunix25 <116853300+Bunix25@users.noreply.github.com>
2023-03-13 11:34:29 -0400

    Update blogRoutes.js

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -48,29 +48,3 @@ router.put('/:id', withAuth, async (req, res) => {
 });
 
 
-// delete blog post
-router.delete('/:id', withAuth, async (req, res) => {
-  try {
-    const blogData = await Blog.destroy({
-      where: {
-        id: req.params.id,
-        user_id: req.session.user_id,
-      },
-      
-    });
-
-    if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
-      return;
-    }
-
-    res.status(200).json(blogData);
-  } catch (err) {
-    console.log('I AM THE ERROR', err);
-    res.status(500).json(err);
-  }
-});
-
-
-module.exports = router;
-
