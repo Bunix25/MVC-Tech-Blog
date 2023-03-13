108a2a00121beb6047d48efa03bd575d81a9ae8c
Bunix25 <svattt7@gmail.com>
2023-03-13 17:46:55 -0400

    conection

--- config/connection.js
+++ config/connection.js
@@ -6,10 +6,11 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
+    host: process.env.DB_HOST,
+    dialect: 'postgres',
+    port: process.env.DB_PORT
   }
 );
+
 
 module.exports = sequelize;
